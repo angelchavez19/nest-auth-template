6b2188b3ea783b4914da52592dc459eb6192ae8b
angelchavez19 <infoangelchavez@gmail.com>
2024-11-02 11:09:35 -0500

    feat(auth): account creation token refresh

--- src/modules/auth/dto/request-refresh-token.dto.ts
+++ src/modules/auth/dto/request-refresh-token.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail } from 'class-validator';
 
-export class RequestChangePasswordDTO {
+export class RequestTokenRefreshDTO {
   @IsEmail({}, { message: 'Invalid email format.' })
   email: string;
 }
